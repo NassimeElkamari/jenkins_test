4fa2dcca5c58d8846cb763dd873021b052ae66e1
Nassime Elkamari <elkamarinassime@gmail.com>
2025-09-08 22:28:29 +0100

    test functionnality

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Nassime Al ostora hh
+          Learn React with Nassime Elkamari
         </a>
       </header>
     </div>
