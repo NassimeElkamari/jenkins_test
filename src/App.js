c123140a424ce24512062c45af2634564bc33b96
Nassime Elkamari <elkamarinassime@gmail.com>
2025-09-16 14:02:48 +0100

    test k8s

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Nassime Elkamari
+          Learn React with Nassime Elkamari !
         </a>
       </header>
     </div>
