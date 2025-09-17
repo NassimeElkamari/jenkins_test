958ba646c5a458677ffae10a4111c1213c5b454e
Nassime Elkamari <elkamarinassime@gmail.com>
2025-09-17 13:17:03 +0100

    test k8s deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Nassime Elkamari
+          Learn React and kubernetes with Nassime Elkamari
         </a>
       </header>
     </div>
